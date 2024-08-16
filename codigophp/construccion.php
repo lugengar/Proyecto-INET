5bc920b4c1533eb68d8c39681f95e460b4fa3dff
lugengar <luciano.barbinii@gmail.com>
2024-08-16 20:04:05 -0300

    a

--- codigophp/construccion.php
+++ codigophp/construccion.php
@@ -16,6 +16,16 @@ function producto($id, $icon, $nombre, $precio){ //CREA EL CUADRO DE UNIVERSIDAD
     </div>
     ');
 }
+function carritoproducto($id, $icon, $nombre, $precio,$cantidad){ //CREA EL CUADRO DE UNIVERSIDAD
+    global $direccionimagen;
+    echo('<div class="productocarrito">
+            <h1 class="nombre">'.$nombre.' x'.$cantidad.'</h1>
+            <p class="precio">Precio: $'.($precio * $cantidad).'</p>
+            <button href="./universidad.php?universidad='.$id.'"class="botonborrar">aaaaaaaaaaaaaaaaaaaaaaa</button>
+            <div class="icono">'.$icon.'</div>
+        </div>
+    ');
+}
 function crearmapa($ubicacion){ //CREA EL MAPA CON LA UBICACION A TRAVEZ DE UNA URL MODIFICADA
     $url = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3287.4006114986105!2d-58.53745522416194!3d-34.51807695298058!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb0946037da75%3A0x7fae4b92e6699b59!2s";
     $url2 = "!5e0!3m2!1ses-419!2sar!4v1684382444792!5m2!1ses-419!2sar";
