53891dae9f4fd47686219dfea1a2e705dc631543
lugengar <luciano.barbinii@gmail.com>
2024-08-23 17:25:51 -0300

    a

--- bypass/local.js
+++ bypass/local.js
@@ -8,8 +8,8 @@ console.log(dato);
 // Redirigir a otra página en un servidor diferente enviando el dato como un nuevo parámetro GET
 
 if (dato) {
-    //const servidorDestino = 'http://localhost/Proyecto-INET/Formularios/crearpedido.php';
-    const servidorDestino = 'https://192.168.0.210/podium/Formularios/crearpedido.php';
+    const servidorDestino = 'http://localhost/Proyecto-INET/Formularios/crearpedido.php';
+    //const servidorDestino = 'https://192.168.0.210/podium/Formularios/crearpedido.php';
     const nuevaURL = `${servidorDestino}?payment_type=${encodeURIComponent(dato)}`;
     window.location.href = nuevaURL;
 }
